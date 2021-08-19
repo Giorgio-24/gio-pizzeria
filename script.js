214e3baef369c548ec2715d06cc390eedc2bf0d0
Giorgio Andrei <cipuandrei@libero.it>
2021-08-19 10:16:50 +0200

    pizzas-menu-added-in-js

--- script.js
+++ script.js
@@ -3,9 +3,294 @@ Vue.config.devtools = true;
 var root = new Vue(
     {
         el: '#root',
-        data: {
-
-        },
+        data: [
+            {
+                "id": 4,
+                "name": "Abarth",
+                "price": "8.50",
+                "white": "0",
+                "special": "0",
+                "ingredients": [
+                    {
+                        "id": 5,
+                        "name": "Brie"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    },
+                    {
+                        "id": 4,
+                        "name": "Zucchine"
+                    }
+                ]
+            },
+            {
+                "id": 5,
+                "name": "Abatantuono",
+                "price": "9.50",
+                "white": "1",
+                "special": "0",
+                "ingredients": [
+                    {
+                        "id": 7,
+                        "name": "Funghi"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 6,
+                        "name": "Prosciutto cotto"
+                    },
+                    {
+                        "id": 8,
+                        "name": "Ricotta"
+                    }
+                ]
+            },
+            {
+                "id": 6,
+                "name": "Agenzia delle entrate",
+                "price": "9.00",
+                "white": "1",
+                "special": "0",
+                "ingredients": [
+                    {
+                        "id": 10,
+                        "name": "Melanzane"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 6,
+                        "name": "Prosciutto cotto"
+                    },
+                    {
+                        "id": 9,
+                        "name": "Salsiccia"
+                    }
+                ]
+            },
+            {
+                "id": 7,
+                "name": "Al Bano",
+                "price": "10.50",
+                "white": "1",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 12,
+                        "name": "Asparagi"
+                    },
+                    {
+                        "id": 5,
+                        "name": "Brie"
+                    },
+                    {
+                        "id": 27,
+                        "name": "Carciofi"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 20,
+                        "name": "Philadelphia"
+                    },
+                    {
+                        "id": 13,
+                        "name": "Spinaci"
+                    }
+                ]
+            },
+            {
+                "id": 8,
+                "name": "Al Pacino",
+                "price": "11.00",
+                "white": "0",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 44,
+                        "name": "Funghi di muschio"
+                    },
+                    {
+                        "id": 63,
+                        "name": "Funghi porcini"
+                    },
+                    {
+                        "id": 54,
+                        "name": "Grana"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 66,
+                        "name": "Petto d'oca"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    },
+                    {
+                        "id": 33,
+                        "name": "Rucola"
+                    }
+                ]
+            },
+            {
+                "id": 9,
+                "name": "Alberto di Monaco",
+                "price": "10.00",
+                "white": "0",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 5,
+                        "name": "Brie"
+                    },
+                    {
+                        "id": 27,
+                        "name": "Carciofi"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 20,
+                        "name": "Philadelphia"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    },
+                    {
+                        "id": 6,
+                        "name": "Prosciutto cotto"
+                    }
+                ]
+            },
+            {
+                "id": 10,
+                "name": "Aldo,Giovanni,Giacomo",
+                "price": "11.00",
+                "white": "1",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 12,
+                        "name": "Asparagi"
+                    },
+                    {
+                        "id": 5,
+                        "name": "Brie"
+                    },
+                    {
+                        "id": 15,
+                        "name": "Gorgonzola"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 20,
+                        "name": "Philadelphia"
+                    },
+                    {
+                        "id": 13,
+                        "name": "Spinaci"
+                    }
+                ]
+            },
+            {
+                "id": 11,
+                "name": "Alesì",
+                "price": "10.00",
+                "white": "0",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 5,
+                        "name": "Brie"
+                    },
+                    {
+                        "id": 27,
+                        "name": "Carciofi"
+                    },
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    },
+                    {
+                        "id": 6,
+                        "name": "Prosciutto cotto"
+                    },
+                    {
+                        "id": 22,
+                        "name": "Stracchino"
+                    }
+                ]
+            },
+            {
+                "id": 12,
+                "name": "MArcoLanci",
+                "price": "10.00",
+                "white": "0",
+                "special": "1",
+                "ingredients": [
+                    {
+                        "id": 28,
+                        "name": "Capperi"
+                    },
+                    {
+                        "id": 14,
+                        "name": "Fagiolini verdi"
+                    },
+                    {
+                        "id": 45,
+                        "name": "Peperoncino"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Margherita",
+                "price": "6.50",
+                "white": "0",
+                "special": "0",
+                "ingredients": [
+                    {
+                        "id": 1,
+                        "name": "Mozzarella"
+                    },
+                    {
+                        "id": 2,
+                        "name": "Pomodoro"
+                    }
+                ]
+            },
+        ],
         methods: {
 
         },
